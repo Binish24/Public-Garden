d8c9a3c61aa6c4debf4137593a5df27adad308bd
nabiullahbenish <nabiullah_benash@yahoo.com>
2024-08-13 14:43:42 -0700

    the first line code has added to the new project.

---
+++ src/EntranceGate/Visitors.java
@@ -0,0 +1,17 @@
+package EntranceGate;
+
+public class Visitors {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Most welcome to the Public-Garden and enjoy it a lot with your nice kids respectful families");
+
+
+
+    }
+
+
+
+}
